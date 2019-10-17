4f75a79b29463f3c788741cfe8fe8157d4dcfa84
Paweł Janik <paweljnk@gmail.com>
2019-10-17 10:23:31 +0200

    Remove handcrafted Client class declaration, it's autogenerated via tsc

--- lib/types.ts
+++ lib/types.ts
@@ -15,13 +15,3 @@ export type GetAuthUriOpts = {
 export type GetTokenOpts = {
   codeVerifier?: string;
 };
-
-export declare class Client {
-  private oauth2Client: ClientOAuth2;
-  private state: string;
-
-  constructor(opts: ClientOpts);
-
-  public getAuthUri(opts: GetAuthUriOpts): Promise<string>;
-  public getToken(callbackUri: string, opts: GetTokenOpts): Promise<ClientOAuth2.Token>;
-}
