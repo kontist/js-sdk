a207dee0668d9f8f14ddc728ad814d44bb47619b
Paweł Janik <paweljnk@gmail.com>
2019-10-25 22:59:28 +0200

    Make Model and Model fetch abstract

--- lib/graphql/model.ts
+++ lib/graphql/model.ts
@@ -3,14 +3,12 @@ import { ResultPage } from "./resultPage";
 import { FetchOptions } from "./types";
 import { IFetch } from "./interfaces";
 
-export class Model<T> implements IFetch<T> {
+export abstract class Model<T> implements IFetch<T> {
   protected client: GraphQLClient;
 
   constructor(graphqlClient: GraphQLClient) {
     this.client = graphqlClient;
   }
 
-  async fetch(args?: FetchOptions): Promise<ResultPage<T>> {
-    throw new Error("Must be overridden by subclasses!");
-  }
+  abstract async fetch(args?: FetchOptions): Promise<ResultPage<T>>;
 }
