7bce7343a87b45adaeb4d0b31c6841a840ac4e5c
tonyskn <tonyskn@gmail.com>
2019-11-12 14:42:47 +0100

    Export errors and IResultPageInterface

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,7 @@
 import { Client } from "./client";
 import * as Constants from "./constants";
 import * as Schema from "./graphql/schema";
+import { IResultPageInterface } from "./graphql/interfaces";
+import * as errors from "./errors";
 
-export { Client, Constants, Schema };
+export { Client, Constants, Schema, IResultPageInterface, errors };
