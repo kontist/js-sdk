8e11243add6d3c0db0f5f99df688543da183e89c
Johannes Pfeiffer <johannespfeiffer@netnexus.de>
2019-11-12 14:52:53 +0100

    Export all interfaces

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,7 @@
 import { Client } from "./client";
 import * as Constants from "./constants";
 import * as Schema from "./graphql/schema";
-import { IResultPageInterface } from "./graphql/interfaces";
-import * as errors from "./errors";
+import * as Interfaces from "./graphql/interfaces";
+import * as Errors from "./errors";
 
-export { Client, Constants, Schema, IResultPageInterface, errors };
+export { Client, Constants, Schema, Interfaces, Errors };
