477d5b42acbee91f271cc614563e1a483514b5d5
Johannes Pfeiffer <johannespfeiffer@netnexus.de>
2019-11-14 09:58:54 +0100

    Move polyfills to the start

--- lib/index.ts
+++ lib/index.ts
@@ -1,11 +1,11 @@
+// polyfills
+(Symbol as any).asyncIterator = Symbol.asyncIterator || Symbol.for("Symbol.asyncIterator");
+
 import { Client } from "./client";
 import * as Constants from "./constants";
 import * as Schema from "./graphql/schema";
 import * as Interfaces from "./graphql/interfaces";
 import * as Types from "./graphql/types";
 import * as Errors from "./errors";
-
-// polyfills
-(Symbol as any).asyncIterator = Symbol.asyncIterator || Symbol.for("Symbol.asyncIterator");
 
 export { Client, Constants, Schema, Interfaces, Types, Errors };
