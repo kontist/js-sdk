91175759dcc98111f503f3a27d5d8d7333c40bfd
Sylvain Boulade <sboulade@gmail.com>
2019-12-19 10:06:55 +0100

    Use enum in tests when appropriate

--- tests/graphql/card.spec.ts
+++ tests/graphql/card.spec.ts
@@ -1,14 +1,14 @@
 import { expect } from "chai";
 import * as sinon from "sinon";
-import { CardType, CardAction } from "../../lib/graphql/schema";
+import { CardType, CardAction, CardStatus } from "../../lib/graphql/schema";
 
 import { Client } from "../../lib";
 import { Card } from "../../lib/graphql/card";
 
 const cardData = {
   id: "010e5dcfdd7949fea50a510e97157168",
-  status: "INACTIVE",
-  type: "VISA_BUSINESS_DEBIT",
+  status: CardStatus.Inactive,
+  type: CardType.VisaBusinessDebit,
   holder: "JEAN DUPONT",
   formattedExpirationDate: "12/22",
   maskedPan: "6802********5119",
@@ -123,7 +123,7 @@ describe("Card", () => {
       // arrange
       const activatedCardData = {
         ...cardData,
-        status: "ACTIVE"
+        status: CardStatus.Active
       };
       const card = new Card(client.graphQL);
       const spyOnRawQuery = sandbox.stub(client.graphQL, "rawQuery").resolves({
@@ -192,8 +192,8 @@ describe("Card", () => {
       // arrange
       const updatedCardData = {
         ...cardData,
-        status: "BLOCKED"
-      }
+        status: CardStatus.Blocked
+      };
       const card = new Card(client.graphQL);
       const spyOnRawQuery = sandbox.stub(client.graphQL, "rawQuery").resolves({
         changeCardStatus: updatedCardData
@@ -209,5 +209,5 @@ describe("Card", () => {
       sinon.assert.calledOnce(spyOnRawQuery);
       expect(result).to.eq(updatedCardData);
     });
-  })
+  });
 });
