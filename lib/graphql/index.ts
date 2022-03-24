b3c568cc6a2462d4c145be6cb4e5ba7c799cb6ee
sinahashemy <sina.hashemy@kontist.com>
2022-03-24 09:08:12 +0100

    Add TaxNumber and BusinessAddress to Client Model

--- lib/graphql/index.ts
+++ lib/graphql/index.ts
@@ -8,6 +8,8 @@ import { User } from "./user";
 import { ChangeRequest } from "./changeRequest";
 import { Declaration } from "./declaration";
 import { Document } from "./document";
+import { TaxNumber } from "./taxNumber";
+import { BusinessAddress } from "./businessAddress";
 
 export const getModels = (graphQLClient: GraphQLClient) => ({
   account: new Account(graphQLClient),
@@ -19,4 +21,6 @@ export const getModels = (graphQLClient: GraphQLClient) => ({
   changeRequest: new ChangeRequest(graphQLClient),
   declaration: new Declaration(graphQLClient),
   document: new Document(graphQLClient),
+  taxNumber: new TaxNumber(graphQLClient),
+  businessAddress: new BusinessAddress(graphQLClient),
 });
