582a00abb1e3a6c4f77b85b81acd015dbc04a894
Pawel Janik <paweljnk@gmail.com>
2023-06-27 14:20:33 +0300

    Add support for custom fields in pagination queries

--- lib/graphql/interfaces.ts
+++ lib/graphql/interfaces.ts
@@ -1,12 +1,26 @@
 import { PageInfo } from "./schema";
+import { FetchOptions } from "./types";
 
-export interface IResultPageInterface<T> {
+export interface IResultPageInterface<
+  T,
+  FetchOptionsType = FetchOptions,
+  RestArgs extends any[] = any[]
+> {
   items: T[];
   pageInfo: PageInfo;
-  nextPage?: () => Promise<IResultPageInterface<T>>;
-  previousPage?: () => Promise<IResultPageInterface<T>>;
+  nextPage?: () => Promise<IResultPageInterface<T, FetchOptionsType, RestArgs>>;
+  previousPage?: () => Promise<
+    IResultPageInterface<T, FetchOptionsType, RestArgs>
+  >;
 }
 
-export interface IFetch<ModelType, FetchOptionsType> {
-  fetch(args?: FetchOptionsType): Promise<IResultPageInterface<ModelType>>;
+export interface IFetch<
+  ModelType,
+  FetchOptionsType,
+  RestArgs extends any[] = any[]
+> {
+  fetch(
+    args?: FetchOptionsType,
+    ...rest: RestArgs
+  ): Promise<IResultPageInterface<ModelType, FetchOptionsType, RestArgs>>;
 }
